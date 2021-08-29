98e86a1a3c4b6f6ff636607312fca2bf47d93487
LenaMorianu <65896439+LenaMorianu@users.noreply.github.com>
2021-08-29 22:20:37 +0200

    Update Incendios_Galicia.py

--- Streamlit/Incendios_Galicia.py
+++ Streamlit/Incendios_Galicia.py
@@ -17,9 +17,9 @@ st.set_page_config( page_title="Incendios en Galicia",
 
 ####### IMAGEN
 
-#image = Image.open('MapaInicio_Galicia.jpg')
+image = Image.open('MapaInicio_Galicia.jpg')
 
-# st.image(image, caption='source: https://github.com/LenaMorianu/ANALISIS-Y-PREDICCION-DE-LOS-INCENDIOS-FORESTALES-EN-GALICIA/blob/main/Streamlit/images/',
+st.image(image, caption='source: https://github.com/LenaMorianu/ANALISIS-Y-PREDICCION-DE-LOS-INCENDIOS-FORESTALES-EN-GALICIA/blob/main/Streamlit/images/',
          use_column_width=True)          
     
 st.title('Análisis y predicción de incendios en Galicia')
