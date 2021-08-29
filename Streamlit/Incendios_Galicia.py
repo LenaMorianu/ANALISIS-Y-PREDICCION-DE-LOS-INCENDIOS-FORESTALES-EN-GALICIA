67c14519ee469fddcd1a44672da642e201feee1b
LenaMorianu <65896439+LenaMorianu@users.noreply.github.com>
2021-08-29 23:50:18 +0200

    Update Incendios_Galicia.py

--- Streamlit/Incendios_Galicia.py
+++ Streamlit/Incendios_Galicia.py
@@ -27,26 +27,28 @@ def importar_datos():
 # Modelos de predicción
 def modelos():
   x = test_size
-  datos_galicia = importar_datos()
+  df = importar_datos()
   
   #Nos quedamos solo con las variables importantes para predecir
   var_imp = ['superficie', 'time_ctrl', 'PRECIPITACION', 'time_ext', 'SOL',
        'personal', 'RACHA', 'lng', 'lat', 'Año', 'TMAX', 'medios',
        'PRES_RANGE','causa']
-  datos_galicia = datos_galicia[var_imp]
+  df = df[var_imp]
   
   #Transformamos la variable target CAUSA a numérica
-  datos_galicia.causa.replace(('causa desconocida','fuego reproducido','intencionado','negligencia','rayo'),(0,1,2,3,4), inplace=True)
+  df.causa.replace(('causa desconocida','fuego reproducido','intencionado','negligencia','rayo'),(0,1,2,3,4), inplace=True)
   
   
   #Dibujar el mapa de correlación
-  corr = datos_galicia.corr()['causa']
+  corr = df.corr()['causa']
 
   return corr
 
 
 
 datos_galicia = importar_datos()
+
+st.write(datos_galicia.head())
 
 # Preguntar por el tamaño del dataset de TEST
 test_size = st.sidebar.slider(label = 'Elige el tamaño del dataset de TEST (%):',
