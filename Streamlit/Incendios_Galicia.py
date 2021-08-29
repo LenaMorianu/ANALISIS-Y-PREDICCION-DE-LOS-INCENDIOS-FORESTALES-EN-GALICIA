b4d07b3c3d1e0097b5b23a2f60e41448c4b32bcf
LenaMorianu <65896439+LenaMorianu@users.noreply.github.com>
2021-08-29 23:48:20 +0200

    Update Incendios_Galicia.py

--- Streamlit/Incendios_Galicia.py
+++ Streamlit/Incendios_Galicia.py
@@ -16,6 +16,54 @@ st.set_page_config( page_title="Incendios en Galicia",
                    initial_sidebar_state="expanded")
 
 
+
+
+# Importar el dataset
+def importar_datos():
+  url = 'https://raw.githubusercontent.com/TFM123456/Big_Data_and_Data_Science_UCM/main/datos_galicia_limpio.csv'
+  return pd.read_csv(url, encoding='ISO-8859-1')
+
+
+# Modelos de predicción
+def modelos():
+  x = test_size
+  datos_galicia = importar_datos()
+  
+  #Nos quedamos solo con las variables importantes para predecir
+  var_imp = ['superficie', 'time_ctrl', 'PRECIPITACION', 'time_ext', 'SOL',
+       'personal', 'RACHA', 'lng', 'lat', 'Año', 'TMAX', 'medios',
+       'PRES_RANGE','causa']
+  datos_galicia = datos_galicia[var_imp]
+  
+  #Transformamos la variable target CAUSA a numérica
+  datos_galicia.causa.replace(('causa desconocida','fuego reproducido','intencionado','negligencia','rayo'),(0,1,2,3,4), inplace=True)
+  
+  
+  #Dibujar el mapa de correlación
+  corr = datos_galicia.corr()['causa']
+
+  return corr
+
+
+
+datos_galicia = importar_datos()
+
+# Preguntar por el tamaño del dataset de TEST
+test_size = st.sidebar.slider(label = 'Elige el tamaño del dataset de TEST (%):',
+                              min_value=0,
+                              max_value=100,
+                              value=15
+                              step=1)
+
+
+
+corr = modelos()
+
+
+
+#################################################################
+
+
 ####### IMAGEN
 
 # image = Image.open('./images/IMG1.jpeg')
@@ -28,87 +76,7 @@ st.title('Análisis y predicción de incendios en Galicia')
 st.write('El presente proyecto tiene como objetivo el análisis de los incendios producidos en Galicia durante el periodo 2001 - 2015, ' +
          'así como realizar predicciones de la CAUSA de incendios con las características (datos) que el usuarios desea consultar.')
          
-         
-         
-# datos_galicia = pd.read.csv('https://raw.githubusercontent.com/LenaMorianu/TFM/main/Galicia_definitivo.csv')
-         
-# Crear los botones del menú  
-inicio = st.sidebar.button("INICIO")
-predicciones = st.sidebar.button("REALIZAR PREDICCIÓN")
 
-         
-st.write('')
-
-
-# Al pulsar el boton inicio
-if inicio:
-  DATA_URL = 'https://raw.githubusercontent.com/LenaMorianu/ANALISIS-Y-PREDICCION-DE-LOS-INCENDIOS-FORESTALES-EN-GALICIA/main/Galicia_definitivo.csv'
-
-  DATE_COLUMN = 'fecha'
-
-  @st.cache
-  def load_data(nrows):
-      data = pd.read_csv(DATA_URL, nrows=nrows)
-      lowercase = lambda x: str(x).lower()
-      data.rename(lowercase, axis='columns', inplace=True)
-      data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-      return data
-
-
-  data_load_state = st.text('Loading data...')
-  data = load_data(100)
-
-  #add_selectbox = st.sidebar.selectbox(
-   #   'Elige la variable que quieres ver:',
-   #   data.columns.values
-   #   )
-
-  st.subheader('Datos de incendios en Galicia')
-  with st.container():
-    if st.checkbox('Muestra los datos'):
-       st.write(data)
-    
-
-  left_column, right_column = st.columns(2)
-
-
-  with left_column:
-      df2 = data['superficie'].copy()
-      st._arrow_line_chart(df2)
-
-
-  with right_column:    
-      st.subheader('Datos de incendios en Galicia')
-      st.write(data)
-    
-   
-  x = data['superficie']
-  y = data['fecha']
-  p = figure(
-      title='Evoluciòn superficie quemada',
-      x_axis_label='Superficie quemada',
-      y_axis_label='Fecha')
-
-  p.line(x, y, legend_label='Evolución', line_width=2)
-  st.bokeh_chart(p, use_container_width=True)
-
-  df2 = data.copy()
-  df2.rename(columns={'lng':'lon'}, inplace=True)
-
-  st.map(df2)
-
-
-  st.write(st.session_state)
-
-# st.write(st.session_state['value']) - ERROR
-         
-
-# Al pulsar el botón predicciones  
-if predicciones:
-  st.sidebar.write('Introduce valores para las siguientes variables:')
-
-
-
-
+  
   
   
