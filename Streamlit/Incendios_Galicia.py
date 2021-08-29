aa89f6faf7e4ccf172b7d46985885cf791dca204
LenaMorianu <65896439+LenaMorianu@users.noreply.github.com>
2021-08-30 01:03:07 +0200

    Update Incendios_Galicia.py

--- Streamlit/Incendios_Galicia.py
+++ Streamlit/Incendios_Galicia.py
@@ -82,8 +82,8 @@ def modelos():
   f1 =  metrics.f1_score(y_test, y_pred, average='weighted')*100
  
   
-  from sklearn.metrics import confusion_matrix, plot_confusion_matrix
-  cm = confusion_matrix(y_test, y_pred)
+  from sklearn.metrics import multilabel_confusion_matrix, plot_confusion_matrix
+  cm = multilabel_confusion_matrix(y_test, y_pred)
   tn, fp, fn, tp = cm.ravel()
   
   
