b6c7801c82a0fb792e448ca5e9a637504d229104
LenaMorianu <65896439+LenaMorianu@users.noreply.github.com>
2021-09-02 19:23:31 +0200

    Update Incendios_Galicia-Copy1.py

--- Streamlit/Incendios_Galicia-Copy1.py
+++ Streamlit/Incendios_Galicia-Copy1.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import bokeh
 from bokeh.plotting import figure
-import pickle
+#import pickle
 from sklearn.metrics import accuracy_score, auc, confusion_matrix, f1_score, precision_score, recall_score, roc_curve
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
