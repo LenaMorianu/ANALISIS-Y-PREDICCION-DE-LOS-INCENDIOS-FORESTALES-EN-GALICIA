4d5bdb35dbb0fb3c38101c45b9c454407054974e
LenaMorianu <65896439+LenaMorianu@users.noreply.github.com>
2021-09-02 19:39:23 +0200

    Update Incendios_Galicia-Copy1.py

--- Streamlit/Incendios_Galicia-Copy1.py
+++ Streamlit/Incendios_Galicia-Copy1.py
@@ -94,7 +94,7 @@ y_proba.columns = y_proba.columns.map({0:'Intencionado',
                                        3:'Fuego_reproducido',
                                        4:'Rayo'}).astype(str)
 
-st.write('La probabilidad de cada observación de pertenecer a las clases de la variable target CAUSA':)
+st.write('La probabilidad de cada observación de pertenecer a las clases de la variable target CAUSA:')
 st.write('')
 st.table(y_proba(head()))
 
